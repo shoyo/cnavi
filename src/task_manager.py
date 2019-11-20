de29cd5209fcef9d2323067985294e586145ca51
Shoyo Inokuchi <shoyoinokuchi@gmail.com>
2019-11-21 01:38:27 +0900

    Refactor task manager

--- src/task_manager.py
+++ src/task_manager.py
@@ -3,26 +3,24 @@ from api import CourseNaviInterface
 
 class TaskManager:
     def __init__(self):
-        pass
+        self.api = CourseNaviInterface()
 
     def pull(self, all=False, verbose=False, debug=False):
         """Pull files from CourseNavi."""
-        cni = CourseNaviInterface()
-
-        dashboard = cni.login()
-        courses = cni.get_courses(dashboard)
+        dashboard = self.api.login()
+        courses = self.api.get_courses(dashboard)
 
         for title, course in courses:
             print(f'> Course title: {title}')
 
-            course_detail = cni.select_course(course, dashboard)
-            lectures = cni.get_lectures(course_detail)
+            course_detail = self.api.select_course(course, dashboard)
+            lectures = self.api.get_lectures(course_detail)
 
             print(f' > Found {len(lectures)} lectures')
 
             for title, lecture in lectures:
                 print(f'  > {title}')
 
-                # files = cni.get_lecture_files(lecture)
+                # files = self.api.get_lecture_files(lecture)
 
 
